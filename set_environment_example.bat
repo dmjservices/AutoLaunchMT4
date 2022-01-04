e916ab51c1f93358cdfbf94ed02bb33761b21797
dmjservices <mrdmjservices@gmail>
2022-01-04 05:59:35 -0800

    updated auto launch

--- set_environment_example.bat
+++ set_environment_example.bat
@@ -4,8 +4,13 @@
 ::# User Environmental Variables
 ::# Check in: Computer Properties -> Advanced System Settings -> Environmental Variables -> User Variables
 
-setx PATH_DSS_Repo "D:\TradingRepos"
+setx PATH_DSS_Repo "C:\Users\devin\Documents\Trading_Repository"
 
-setx PATH_T1_T "D:\FxPro - Terminal1"
-setx PATH_T3_T "D:\FxPro - Terminal3"
+setx PATH_T1_T "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (1)"
+setx PATH_T2_T "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (2)"
+setx PATH_T3_T "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (3)"
+setx PATH_T4_T "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)"
+
+
+::setx PATH_T3_T "D:\FxPro - Terminal3"
 
