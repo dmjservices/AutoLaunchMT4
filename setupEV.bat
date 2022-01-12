7779ed5e71551f7afd9679106f6dbadb26e7d8b4
dmjservices <mrdmjservices@gmail>
2022-01-12 11:24:23 -0800

    Create setupEV.bat

---
+++ setupEV.bat
@@ -0,0 +1,26 @@
+::# ----------------------------------------------------------------------------------------
+::# Environmental Variables for Lazytrade System
+::# ----------------------------------------------------------------------------------------
+::# User Environmental Variables
+::# Check in: Computer Properties -> Advanced System Settings -> Environmental Variables -> User Variables
+
+::# Customize path to the user folder where all relevantrepositories should be located
+setx PATH_DSS_Repo "%USERPROFILE%\Documents\Trading_Repository"
+
+::# Customize path to the user folders with Trading Platforms
+setx PATH_T1 "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (1)\MQL4\Files"
+setx PATH_T1_E "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (1)\MQL4\Experts"
+setx PATH_T1_I "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (1)\MQL4\Include"
+
+setx PATH_T2 "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (2)\MQL4\Files"
+setx PATH_T2_E "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (2)\MQL4\Experts"
+setx PATH_T2_I "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (2)\MQL4\Include"
+
+setx PATH_T3 "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (3)\MQL4\Files"
+setx PATH_T3_E "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (3)\MQL4\Experts"
+setx PATH_T3_I "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (3)\MQL4\Include"
+
+setx PATH_T4 "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\MQL4\Files"
+setx PATH_T4_E "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\MQL4\Experts"
+setx PATH_T4_I "C:\Oanda - Terminals\OANDA - MetaTrader Portable - (4)\MQL4\Include"
+
